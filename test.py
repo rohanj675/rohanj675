d31410b69f4a7fb574626e1acc462c8749d9d98b
Your Name <you@example.com>
2024-12-28 18:34:28 +0530

    new added

---
+++ test.py
@@ -0,0 +1,13 @@
+
+print("ADDition of two numbers")
+
+a=int(input("Enter the Number 1st ->"))
+b=int(input("Enter the Number 2nd->"))
+c= a+b              
+print("sum is -> ",c)5
+7
+
+
+a,b = b,a
+print("swapping ->",a,b)
+
